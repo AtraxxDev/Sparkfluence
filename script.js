bae2fa9a15102bf8c7db2ae4aa044f0976dcaba9
Elliot Reyes <gametraxworks@gmail.com>
2024-11-09 00:33:56 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('cta-btn').addEventListener('click', function() {
+    window.location.href = "#services";  // Desplazarse a la sección de servicios
+});
+
+document.getElementById('contact-btn').addEventListener('click', function() {
+    alert("¡Gracias por tu interés! Nos pondremos en contacto pronto.");
+});
